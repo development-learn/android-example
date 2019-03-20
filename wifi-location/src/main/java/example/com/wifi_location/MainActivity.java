ead101a259d7fe10e0f69cb66e4588422ba86828
sunnyliuqi <liu963277983>
2019-03-20 15:16:43 +0800

    三边定位算法+误差优化

--- wifi-location/src/main/java/example/com/wifi_location/MainActivity.java
+++ wifi-location/src/main/java/example/com/wifi_location/MainActivity.java
@@ -1,13 +1,135 @@
 package example.com.wifi_location;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import example.com.wifi_location.service.PointService;
+import example.com.wifi_location.vo.PointInfo;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PointService pointService;
+    private Button btRefresh;
+    private List<PointInfo> pointInfos;
+    private MyAdapt myAdapt;
+
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pointService=new PointService();
+        wifiManager= (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+
+        pointInfos = pointService.getPointInfos();
+
+        btRefresh = findViewById(R.id.bt_refresh);
+        btRefresh.setOnClickListener(v -> {
+            refresh(v);
+        });
+
+        ListView listView = findViewById(R.id.lv_wifi);
+        myAdapt = new MyAdapt();
+        listView.setAdapter(myAdapt);
+
+
+    }
+
+    /**
+     * 刷新操作
+     *
+     * @param view
+     */
+    public void refresh(View view) {
+        pointInfos = pointService.getPointInfos();
+        myAdapt.notifyDataSetChanged();
+
+    }
+
+    public class MyAdapt extends BaseAdapter {
+
+        /**
+         * How many items are in the data set represented by this Adapter.
+         *
+         * @return Count of items.
+         */
+        @Override
+        public int getCount() {
+            return pointInfos.size();
+        }
+
+        /**
+         * Get the data item associated with the specified position in the data set.
+         *
+         * @param position Position of the item whose data we want within the adapter's
+         *                 data set.
+         * @return The data at the specified position.
+         */
+        @Override
+        public Object getItem(int position) {
+            return pointInfos.get(position);
+        }
+
+        /**
+         * Get the row id associated with the specified position in the list.
+         *
+         * @param position The position of the item within the adapter's data set whose row id we want.
+         * @return The id of the item at the specified position.
+         */
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        /**
+         * Get a View that displays the data at the specified position in the data set. You can either
+         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+         * parent View (GridView, ListView...) will apply default layout parameters unless you use
+         * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+         * to specify a root view and to prevent attachment to the root.
+         *
+         * @param position    The position of the item within the adapter's data set of the item whose view
+         *                    we want.
+         * @param convertView The old view to reuse, if possible. Note: You should check that this view
+         *                    is non-null and of an appropriate type before using. If it is not possible to convert
+         *                    this view to display the correct data, this method can create a new view.
+         *                    Heterogeneous lists can specify their number of view types, so that this View is
+         *                    always of the right type (see {@link #getViewTypeCount()} and
+         *                    {@link #getItemViewType(int)}).
+         * @param parent      The parent that this view will eventually be attached to
+         * @return A View corresponding to the data at the specified position.
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if (convertView == null) {
+                view = View.inflate(getApplicationContext(), R.layout.item_wifi, null);
+            } else {
+                view = convertView;
+            }
+            PointInfo info = pointInfos.get(position);
+            TextView name = view.findViewById(R.id.item_tv_name);
+            name.setText(info.getName());
+
+            TextView xy = view.findViewById(R.id.item_tv_xy);
+            xy.setText(info.getX() + "," + info.getY());
+
+            TextView distance = view.findViewById(R.id.item_tv_distance);
+            distance.setText(String.valueOf(info.getD()));
+
+            return view;
+        }
     }
 }
--- wifi-location/src/test/java/example/com/wifi_location/ExampleUnitTest.java
+++ wifi-location/src/test/java/example/com/wifi_location/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package example.com.wifi_location;
 
+import example.com.wifi_location.algorithm.TrilaterationLocationAlgorithm;
+import example.com.wifi_location.vo.LocationInfo;
+import example.com.wifi_location.vo.PointInfo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +17,27 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void algorithm() {
+        TrilaterationLocationAlgorithm trilaterationLocationAlgorithm=new TrilaterationLocationAlgorithm();
+
+        PointInfo p1=new PointInfo("w1",3,0,3);
+        PointInfo p2=new PointInfo("w2",0,4,4);
+        PointInfo p3=new PointInfo("w3",3,4,5);
+        PointInfo p4=new PointInfo("w4",0,-9,10);
+//        PointInfo p5=new PointInfo("w5",1,0,1);
+//        PointInfo p6=new PointInfo("w6",0,2,2);
+        PointInfo[] ps=new PointInfo[6];
+        ps[0]=p1;
+        ps[1]=p2;
+        ps[2]=p3;
+        ps[3]=p4;
+//       ps[4]=p5;
+//        ps[5]=p6;
+            LocationInfo locationInfo = trilaterationLocationAlgorithm.getLogcation(ps);
+            System.out.println("*******x坐标："+locationInfo.getxAxis()+";y坐标："+locationInfo.getyAxis()+"*******");
+
     }
 }
