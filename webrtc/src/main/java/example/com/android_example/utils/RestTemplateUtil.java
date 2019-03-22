c228db6628119fa63a04311a46bdf7c1749d7159
sunnyliuqi <liu963277983>
2019-03-22 11:49:08 +0800

    地址更换

--- webrtc/src/main/java/example/com/android_example/utils/RestTemplateUtil.java
+++ webrtc/src/main/java/example/com/android_example/utils/RestTemplateUtil.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
  */
 public class RestTemplateUtil {
     private static RestTemplate restTemplate;
-    public final static String HOST = "192.168.31.134:2019/websocket";
+    public final static String HOST = "10.110.1.11:2019/websocket";
     private final static String API = "https://" + HOST;
     public static final String LOGIN = "/loginjwt";
     public static final String MY_FRIENDS = "/myfriends";
